b4cedbccfcebbdffef4d5e5d891e6334be4c53a3
Tazeem Khan <67814275+TazeemKhan9@users.noreply.github.com>
2022-05-04 00:30:27 -0700

    Add files via upload

--- train.py
+++ train.py
@@ -1,53 +1,37 @@
+from pprint import pprint
+
+import lightgbm as lgb
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+
 import mlflow
-import json
-import pandas as pd
-import numpy as np
-from prophet import Prophet, serialize
-from prophet.diagnostics import cross_validation, performance_metrics
+import mlflow.lightgbm
 
-SOURCE_DATA = (
-    "https://raw.githubusercontent.com/facebook/prophet/master/examples/example_retail_sales.csv"
-)
-ARTIFACT_PATH = "model"
-np.random.seed(12345)
+from utils import fetch_logged_data
 
 
-def extract_params(pr_model):
-    return {attr: getattr(pr_model, attr) for attr in serialize.SIMPLE_ATTRIBUTES}
+def main():
+    # prepare example dataset
+    X, y = load_iris(return_X_y=True, as_frame=True)
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    # enable auto logging
+    # this includes lightgbm.sklearn estimators
+    mlflow.lightgbm.autolog()
+
+    regressor = lgb.LGBMClassifier(n_estimators=20, reg_lambda=1.0)
+    regressor.fit(X_train, y_train, eval_set=[(X_test, y_test)])
+    y_pred = regressor.predict(X_test)
+    f1 = f1_score(y_test, y_pred, average="micro")
+    run_id = mlflow.last_active_run().info.run_id
+    print("Logged data and model in run {}".format(run_id))
+
+    # show logged data
+    for key, data in fetch_logged_data(run_id).items():
+        print("\n---------- logged {} ----------".format(key))
+        pprint(data)
 
 
-sales_data = pd.read_csv(SOURCE_DATA)
-
-with mlflow.start_run():
-
-    model = Prophet().fit(sales_data)
-
-    params = extract_params(model)
-
-    metric_keys = ["mse", "rmse", "mae", "mape", "mdape", "smape", "coverage"]
-    metrics_raw = cross_validation(
-        model=model,
-        horizon="365 days",
-        period="180 days",
-        initial="710 days",
-        parallel="threads",
-        disable_tqdm=True,
-    )
-    cv_metrics = performance_metrics(metrics_raw)
-    metrics = {k: cv_metrics[k].mean() for k in metric_keys}
-
-    print(f"Logged Metrics: \n{json.dumps(metrics, indent=2)}")
-    print(f"Logged Params: \n{json.dumps(params, indent=2)}")
-
-    mlflow.prophet.log_model(model, artifact_path=ARTIFACT_PATH)
-    mlflow.log_params(params)
-    mlflow.log_metrics(metrics)
-    model_uri = mlflow.get_artifact_uri(ARTIFACT_PATH)
-    print(f"Model artifact logged to: {model_uri}")
-
-
-loaded_model = mlflow.prophet.load_model(model_uri)
-
-forecast = loaded_model.predict(loaded_model.make_future_dataframe(60))
-
-print(f"forecast:\n${forecast.head(30)}")
+if __name__ == "__main__":
+    main()
